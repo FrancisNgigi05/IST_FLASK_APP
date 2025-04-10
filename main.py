b9cf092d58064093db8de59ab92b1de06695721d
toxicanonymous253 <ngugifrancis930@gmail.com>
2025-04-10 10:29:41 +0300

    Done

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, request
+import os
+
 
 app = Flask(__name__)
 
@@ -31,4 +33,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
